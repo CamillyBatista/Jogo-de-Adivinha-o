b8c2d51ce8a3a4000251198de30454f86e41d956
Camilly <Camillybatistaa18@gmail.com>
2026-02-24 11:41:00 +0000

    melhora do jogo

--- js.js
+++ js.js
@@ -10,10 +10,13 @@
 */
 function gerarNumerosecreto(){
     const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
-    console.log(random(0,11));
+    return random (1,11)
 };
+
 let numerosecreto = gerarNumerosecreto();
 const obterelemento = (id) => document.getElementById(id);
+let tentativas = 0;
+let Maxtentativas = 5;
 
 function obterchutedousuario(){
     const input = obterelemento('chute');
@@ -21,8 +24,27 @@ function obterchutedousuario(){
     
 }
 function jogar(){
-    const chute = obterchutedousuario();
-    console.log(chute);
+    if (tentativas >= Maxtentativas){
+        document.getElementById("resultado").textContent = "Suas tentativas Acabaram";
+        return;
+    }
 
 
+const chute = obterchutedousuario();
+tentativas++;
+
+if (chute === numerosecreto){
+    document.getElementById("resultado").textContent="Parabéns você acertou";
+}
+ else if(chute < numerosecreto){
+    document.getElementById("resultado").textContent="Errado, é maior!";
+ } else {
+    document.getElementById("resultado").textContent="Muito Alto!";
+ }
+
+ if (tentativas === Maxtentativas && chute != numerosecreto){
+    document.getElementById("resultado").textContent="Suas chances acabaram, o numero era " + numerosecreto;
+ }
+ document.getElementById("dica").textContent =
+    `Tentativas: ${tentativas} de ${Maxtentativas}`;
 }
